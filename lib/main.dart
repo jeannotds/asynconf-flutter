b66055da030a7a1c494c07aca35788c4fc85f059
jeannot diambu <diambujeannot@gmail.com>
2024-07-01 15:23:51 +0200

    import image

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,31 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text("Asyncof 2022"),
           ),
-          body: const Center(
-            child: Text("Bienvenue sur l'App du salon"),
+          body: Center(
+            child: Container(
+                padding: const EdgeInsets.all(8),
+                decoration: const BoxDecoration(),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 120,
+                      width: 120,
+                      child: Image.asset(
+                          "assets/images/chapeau-de-remise-de-diplome.png"),
+                    ),
+                    const Text(
+                      "Asyncof 2022",
+                      style:
+                          TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+                    ),
+                    const Text(
+                      "Salon virtuel de l'informatique. D 27 au 29 Octobre 2024",
+                      style: TextStyle(fontSize: 24),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                )),
           )),
     );
   }
