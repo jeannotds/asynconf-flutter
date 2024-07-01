af2c7872d6c336e55dda2769865da2b992f3584a
jeannot diambu <diambujeannot@gmail.com>
2024-07-01 15:40:19 +0200

    create page events

--- lib/main.dart
+++ lib/main.dart
@@ -13,3 +13,16 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(home: HomePage());
   }
 }
+
+class EventPage extends StatelessWidget {
+  const EventPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Planning du salon"),
+      ),
+    );
+  }
+}
