68fcfd05665cdc649c31ff35272b02e360228e72
jeannot diambu <diambujeannot@gmail.com>
2024-07-01 17:26:31 +0200

    use state full

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({super.key});
 
+  @override
+  State<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,26 +23,6 @@ class EventPage extends StatelessWidget {
                 ),
                 title: const Text('Arnaud Garnacho'),
                 subtitle: const Text('Git blame --no-offense ?'),
-                trailing: const Icon(Icons.more_vert),
-                isThreeLine: true,
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Image.asset("assets/images/john.jpg"),
-                title: const Text('John Margon'),
-                subtitle: const Text(
-                    'A sufficiently long subtitle warrants three lines.'),
-                trailing: const Icon(Icons.more_vert),
-                isThreeLine: true,
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Image.asset("assets/images/james.jpg"),
-                title: const Text('James Parcaston'),
-                subtitle: const Text(
-                    'A sufficiently long subtitle warrants three lines.'),
                 trailing: const Icon(Icons.more_vert),
                 isThreeLine: true,
               ),
