2f8f905514b80e46994e78368b52061d7c66de54
jeannot diambu <diambujeannot@gmail.com>
2024-07-02 11:37:52 +0200

    create navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Asynconf 2024"),
+        ),
+        body: const HomePage(),
+        bottomNavigationBar: BottomNavigationBar(
+          //Here is for styles
+          selectedItemColor: Colors.green,
+
+          //Items of navigation
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Accueil"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_month), label: "Planning"),
+          ],
+        ),
+      ),
+    );
   }
 }
