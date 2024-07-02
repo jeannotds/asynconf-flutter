fd86b92fe1fcccd7ade65b4109efbc4ee81c18ce
jeannot diambu <diambujeannot@gmail.com>
2024-07-02 11:05:33 +0200

    display events

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -8,26 +8,53 @@ class EventPage extends StatefulWidget {
 }
 
 class _EventPageState extends State<EventPage> {
+  final events = [
+    {
+      "speaker": "Jeannot ds",
+      "avatar": "assets/images/john.jpg",
+      "subject": "Show me your wisdom",
+      "date": "13h00 à 14h"
+    },
+    {
+      "speaker": "Beni sm",
+      "avatar": "assets/images/james.jpg",
+      "subject": "The way of glory",
+      "date": "10h20 à 15h"
+    },
+    {
+      "speaker": "Levis ds",
+      "avatar": "assets/images/arnaud.jpg",
+      "subject": "See me",
+      "date": "14h45 à 18h"
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Planning du salon"),
         ),
-        body: ListView(
-          children: [
-            Card(
-              child: ListTile(
-                leading: Image.asset(
-                  "assets/images/arnaud.jpg",
-                ),
-                title: const Text('Arnaud Garnacho'),
-                subtitle: const Text('Git blame --no-offense ?'),
-                trailing: const Icon(Icons.more_vert),
-                isThreeLine: true,
-              ),
-            ),
-          ],
+        body: Center(
+          child: ListView.builder(
+              itemCount: events.length,
+              itemBuilder: (context, index) {
+                final event = events[index];
+                final speaker = event["speaker"];
+                final avatar = event["avatar"];
+                final date = event["date"];
+                final subject = event["subject"];
+                return Card(
+                  child: ListTile(
+                    leading: Image.asset(
+                      "$avatar",
+                    ),
+                    title: Text('$speaker ($date)'),
+                    subtitle: Text('$subject'),
+                    trailing: Icon(Icons.more_vert),
+                    isThreeLine: true,
+                  ),
+                );
+              }),
         ));
   }
 }
